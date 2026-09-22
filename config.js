d6a8eb6c85b00c4259a4a4ef4faa6900a5b1e927
NelsonTavares <97975718+NelsonTavares@users.noreply.github.com>
2026-09-22 18:28:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://ygubvhnmhqnfiyzuvgmo.supabase.co
-",
+  url: "https://ygubvhnmhqnfiyzuvgmo.supabase.co",
   publishableKey: "sb_publishable_pWIQdeMun1mPqxIzPvphrw_pEaDE5Hw"
 };
